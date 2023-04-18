5dc452a2b361aec2cff47d19182354cd6a74bc5b
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-04-18 09:41:21 -0500

    FAB and dialog to add task (design)

--- app/src/main/java/com/navi/taskapp/addtasks/ui/TaskScreen.kt
+++ app/src/main/java/com/navi/taskapp/addtasks/ui/TaskScreen.kt
@@ -1,8 +1,79 @@
 package com.navi.taskapp.addtasks.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Preview(showSystemUi = true)
+@Composable
+fun TaskScreen() {
+    Box(Modifier.fillMaxSize()) {
+        AddTaskDialog(true, onDismiss = {
+
+            }, onTaskAdded = {
+
+            })
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+}
 
 @Composable
-fun TaskScreen(taskViewModel: TaskViewModel) {
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(onClick = {
+    }, modifier = modifier.padding(16.dp)) {
+        Icon(Icons.Filled.Add, contentDescription = "add button")
+    }
+}
 
+@Composable
+fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var myTasks by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(12.dp)
+            ) {
+                Text(
+                    text = "Add your task",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = myTasks,
+                    onValueChange = { myTasks = it },
+                    singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = {
+                    onTaskAdded(myTasks)
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Add task")
+                }
+            }
+        }
+    }
 }
